63f70e26044394635e1056d2212a066ef4b4d5ca
Andreas Stassivik <andreas@stassi.net>
2020-02-13 18:06:50 -0800

    test 0-distance ranges

--- test/range.js
+++ test/range.js
@@ -3,6 +3,18 @@ import { expect } from 'chai'
 import { range } from '../src'
 
 describe('#range', () => {
+  describe('0-distance ranges', () => {
+    it('should return an empty array', () => {
+      expect(
+        range({ maximum: 0, minimum: 0 })
+      ).to.be.an('array').that.has.a.lengthOf(0)
+
+      expect(
+        range({ minimum: 5, maximum: 5 })
+      ).to.be.an('array').that.has.a.lengthOf(0)
+    })
+  })
+
   describe('1-distance ranges', () => {
     it('should return the minimum', () => {
       expect(range({ minimum: 0, maximum: 1 })[0]).to.equal(0)
