3248a8dc1ae0a29e2d09ffcd73b9331d776eb9b9
Andreas Stassivik <andreas@stassi.net>
2020-02-28 16:11:09 -0800

    deprecation warning

--- src/cardinality.js
+++ src/cardinality.js
@@ -1,4 +1,8 @@
-// TODO: Split into cardinality and length
-const cardinality = x => x.length
+const cardinality = x => {
+  // TODO: Implement set-based cardinality (ignore duplicates)
+  console.warn('[neida] deprecated method: cardinality ')
+
+  return x.length
+}
 
 export default cardinality
