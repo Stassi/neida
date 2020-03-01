dadc7cff70e3e7b56d7c65f9001cec5a2b3b270a
Andreas Stassivik <andreas@stassi.net>
2020-03-01 14:43:58 -0800

    jsdoc add

--- src/add.js
+++ src/add.js
@@ -1,3 +1,13 @@
-const add = (x, y) => x + y
-
-export default add
+/**
+ * @description Add two values.
+ * @example
+ * sum(2, 3)
+ * // => 5
+ * @module
+ * @param {number} x
+ * @param {number} y
+ * @returns {number} sum of x and y
+ */
+export default function add (x, y) {
+  return x + y
+}
