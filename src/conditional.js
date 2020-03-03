7f0b842cfa2b0d1964956cf385a1cea74706249e
Andreas Stassivik <andreas@stassi.net>
2020-03-02 18:00:40 -0800

    document conditional

--- src/conditional.js
+++ src/conditional.js
@@ -1,9 +1,31 @@
-const conditional = ({
-  ifFalse,
-  ifTrue,
-  predicate
-}) => predicate()
-  ? ifTrue()
-  : ifFalse()
+/**
+ * @callback predicate
+ * @description Determines the outcome of a decision.
+ * @returns {boolean}
+ */
 
-export default conditional
+/**
+ * @callback callConditionally
+ * @description A function that is called only if a predicate is satisfied.
+ * @returns {*}
+ */
+
+/**
+ * @description Conditionally call a function.
+ * @example
+ * conditional({
+ *   ifFalse: () => 'falsy',
+ *   ifTrue: () => 'truthy',
+ *   predicate: () => true
+ * })
+ * // => 'truthy'
+ * @module
+ * @param {Object} o
+ * @param {callConditionally} o.ifFalse Called if predicate returns `false`.
+ * @param {callConditionally} o.ifTrue Called if predicate returns `true`.
+ * @param {predicate} o.predicate Returned value determines which function is called.
+ * @returns {*} Value returned by called function.
+ */
+export default function conditional ({ ifFalse, ifTrue, predicate }) {
+  return predicate() ? ifTrue() : ifFalse()
+}
